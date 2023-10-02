a5f184cf979ac23585b4898bf023967930853e47
Sanisetty kartheek <kartheek@KartheeettysAir.lan>
2023-10-01 23:23:38 -0400

    week6

---
+++ src/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+
+    <div className="bottom">
+    <div className="center">
+        {/* <!-- This is Semantic HTML Change --> */}
+        <footer>
+            <p>Kartheek Sanisetty-801348859</p>
+          </footer>
+    </div>
+</div>
+  
+    
+  );
+}
+
+export default Footer;
